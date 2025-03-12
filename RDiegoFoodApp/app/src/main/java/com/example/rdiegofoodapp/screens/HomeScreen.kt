31975aff4889d7f2777a99b2fb5e39cd67da8b32
Diego Sebastian Rojas Guerrero <diegorojas13579@gmail.com>
2025-03-11 18:34:29 -0600

    Primer commit

---
+++ RDiegoFoodApp/app/src/main/java/com/example/rdiegofoodapp/screens/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.rdiegofoodapp.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(innerPadding: PaddingValues){
+
+}
